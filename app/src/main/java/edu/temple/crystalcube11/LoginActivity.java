0f2f51b19477a16953d669f1f44583fab4a5aafa
Hong <hhongear@gmail.com>
2017-03-11 15:17:18 -0500

    get/post request login screen
    get/post request login screen

--- app/src/main/java/edu/temple/crystalcube11/LoginActivity.java
+++ app/src/main/java/edu/temple/crystalcube11/LoginActivity.java
@@ -1,14 +1,41 @@
 package edu.temple.crystalcube11;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.Iterator;
+
+import javax.net.ssl.HttpsURLConnection;
 
 public class LoginActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,19 +50,40 @@ public class LoginActivity extends AppCompatActivity {
         final String username = usernameET.getText().toString().trim();
         final String password = passwordET.getText().toString().trim();
 
+        final TextView mTextView = (TextView) findViewById(R.id.text);
+
+
         loginBT.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (true) { // TODO: 2/2/17 verify credential with db
-                    // go to CrystalMainActivity
-                    Intent myIntent = new Intent(LoginActivity.this, CrystalMainActivity.class);
-                    startActivity(myIntent);
-                } else {
-                    // TODO: 2/2/17 make EditText red and shake indicating invalid credential
-                }
+                // Instantiate the RequestQueue.
+                RequestQueue queue = Volley.newRequestQueue(LoginActivity.this);
+                String url ="http://34.207.95.183";
+
+                // Request a string response from the provided URL.
+                StringRequest stringRequest = new StringRequest(Request.Method.POST, url,
+                        new Response.Listener<String>() {
+                            @Override
+                            public void onResponse(String response) {
+                                // Display the first 500 characters of the response string.
+                                Toast.makeText(LoginActivity.this, response, Toast.LENGTH_SHORT).show();
+                            }
+                        }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        mTextView.setText("That didn't work!");
+                    }
+                });
+                // Add the request to the RequestQueue.
+                queue.add(stringRequest);
+
             }
         });
 
-
     }
+
+
+
+
 }
+
