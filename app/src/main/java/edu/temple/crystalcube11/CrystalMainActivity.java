4fa4547f55701445ea377b13a8318bbe0ccb0139
Hong <hhongear@gmail.com>
2017-04-10 14:12:17 -0400

    front camera enabled, capture face, send face and name to create account, read main user id for cube from firebase

--- app/src/main/java/edu/temple/crystalcube11/CrystalMainActivity.java
+++ app/src/main/java/edu/temple/crystalcube11/CrystalMainActivity.java
@@ -3,21 +3,104 @@ package edu.temple.crystalcube11;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 public class CrystalMainActivity extends AppCompatActivity {
+    private String TAG = "onDataChange";
+
+    private EditText userID_ET;
+    private EditText user_first_name_ET;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crystal_main);
 
+        // read firebase data =========================
+        // Get a reference to our users
+        userID_ET = (EditText) findViewById(R.id.user_id_edit_text);
+        user_first_name_ET = (EditText) findViewById(R.id.user_name_edit_text);
+
+        final FirebaseDatabase database = FirebaseDatabase.getInstance();
+        final DatabaseReference ref = database.getReference("crystalCubes/crystal_chan_6/user");
+
+        // Attach a listener to read the data at our posts reference
+        ref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // pull all data of current user
+                User user = dataSnapshot.getValue(User.class);
+                Log.d(TAG, String.valueOf(user));
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Log.d(TAG ,"The read failed: " + databaseError.getCode());
+            }
+        });
+
+        ref.orderByChild("profile").limitToFirst(1).addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+
+                String userID = String.valueOf(dataSnapshot.getKey());
+                Log.d(TAG + " userId key", userID);
+                userID_ET.setText(String.valueOf(userID));
+
+                ref.child("profile/firstName").addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        String user_firstname = String.valueOf(dataSnapshot.getValue());
+                        user_first_name_ET.setText(user_firstname);
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+
+                    }
+                });
+
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+
+
+        // adapter view for drop down ===================================
         Spinner mySpinner= (Spinner) findViewById(R.id.spinner_view);
         String[] crystalFunctions = {"Home", "Reminder", "Music", "To-do List", "Weather"};
 
@@ -34,8 +117,6 @@ public class CrystalMainActivity extends AppCompatActivity {
                 //todo create activity for each functions
                 //Toast.makeText(CrystalMainActivity.this, ((TextView) view).getText().toString(), Toast.LENGTH_SHORT).show();
                 //Toast.makeText(CrystalMainActivity.this, ""+i , Toast.LENGTH_SHORT).show();
-
-
 
                 Intent mIntent;
 
--- app/src/main/java/edu/temple/crystalcube11/WeatherActivity.java
+++ app/src/main/java/edu/temple/crystalcube11/WeatherActivity.java
@@ -1,13 +1,75 @@
 package edu.temple.crystalcube11;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.Calendar;
+import java.util.Date;
 
 public class WeatherActivity extends AppCompatActivity {
+
+    private static final String TAG = "NewPostActivity";
+
+    private TextView mTextView;
+
+    private Button postBT;
+
+    private DatabaseReference myRef;
+    private FirebaseDatabase database;
+
+    private Date mTime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weather);
+
+
+        database = FirebaseDatabase.getInstance();
+        mTime = Calendar.getInstance().getTime();
+        myRef = database.getReference("message"+mTime);
+
+        // write to db
+        postBT = (Button) findViewById(R.id.postBTid);
+        postBT.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                myRef.setValue("Hello, World! "+mTime);
+            }
+        });
+
+        mTextView = (TextView) findViewById(R.id.textView);
+
+        // Read from the database
+        //myRef = database.getReference("users");
+        //todo handle hashmap data from firebase
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                String value = dataSnapshot.getValue(String.class);
+                Log.d(TAG, "Value is: " + value);
+
+                mTextView.setText(value);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w(TAG, "Failed to read value.", error.toException());
+            }
+        });
+
     }
 }
